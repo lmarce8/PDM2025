54bcb23b91ee4aadb20745e09bb0081659e772f1
lmarce8 <143825337+lmarce8@users.noreply.github.com>
2025-01-29 02:19:26 -0600

    Assignment 1 Example 1

---
+++ Example-01/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(120,100,100); //lime green background
+
+  circle(100,100,100);
+  square(175,50,100);
+
+}
